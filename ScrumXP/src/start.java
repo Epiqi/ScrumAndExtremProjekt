7d9034fb8698d9ee97fec52f4ac921ebe31994cf
AndreasAKD <81685640+AndreasAKD@users.noreply.github.com>
2021-04-13 09:53:00 +0200

    Uppdatera

--- ScrumXP/src/start.java
+++ ScrumXP/src/start.java
@@ -14,7 +14,19 @@ public class start {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+<<<<<<< Updated upstream
         // TODO code application logic here
+=======
+        try {
+
+            scrumXPdb = new InfDB("mibdb", "3306", "mibdba", "mibkey");
+            //new Login(scrumXPdb).setVisible(true);
+            new HomePage(scrumXPdb,"user").setVisible(true);
+
+        } catch (InfException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+>>>>>>> Stashed changes
     }
     
 }

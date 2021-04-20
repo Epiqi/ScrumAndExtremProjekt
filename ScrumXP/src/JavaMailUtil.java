3e05b71f7b61d2dcfa30b879ec9b97f3137b52ec
efegu <eg524729@gmail.com>
2021-04-20 10:08:11 +0200

    Update JavaMailUtil.java
    Update

--- ScrumXP/src/JavaMailUtil.java
+++ ScrumXP/src/JavaMailUtil.java
@@ -4,7 +4,7 @@ import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
 /**
- *
+ * @author Efe
  * @author Magnus
  */
 public class JavaMailUtil {
@@ -18,7 +18,7 @@ public class JavaMailUtil {
         prop.put("mail.smtp.host", "smtp.gmail.com");
         prop.put("mail.smtp.port", "587");
         prop.put("mail.smtp.auth", "true");
-        prop.put("mail.smtp.starttls.enable", "true"); //TLS
+        prop.put("mail.smtp.starttls.enable", "true");
         prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
         
         Session session = Session.getInstance(prop,
